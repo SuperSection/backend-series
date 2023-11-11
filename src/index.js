2c3ebaee679e45a04667a1fa2268384f6e8325a0
SuperSection <soumosarkar297@gmail.com>
2023-11-11 13:00:27 +0530

    connect to MongoDB Atlas - professional approach

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-import express from "express";
-const app = express();
+dotenv.config({
+  path: "./env",
+});
 
+connectDB();
+
+
+/*
 ; (async () => {
   try {
       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
@@ -20,3 +26,4 @@ const app = express();
     throw error;
   }
 })();
+*/
