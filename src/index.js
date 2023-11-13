c79ee062958e0c31d3f28eb4ac3a7694dfbf5bf3
SuperSection <soumosarkar297@gmail.com>
2023-11-13 18:27:26 +0530

    utils: custom API response & Error handling

--- src/index.js
+++ src/index.js
@@ -6,24 +6,17 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-
-/*
-; (async () => {
-  try {
-      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-      app.on("error", (error) => {
-          console.log("ERROR: ", error);
-          throw error
-      })
-
-      app.listen(process.env.PORT, () => {
-          console.log(`App is listening on port ${process.env.PORT}`);
-      })
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error
+    });
+    
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`• Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection Failed !!! ", err);
+  });
